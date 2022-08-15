597bb8ce2478e754c4d950273e4613dbb7542be2
Maria Gabriel <marialgk@gmail.com>
2022-08-15 17:22:44 -0300

    Update AnalyseNNNS.py
    Added feature: check if variables scores are within expected range.
    Corrected bug in the Binarify function.

--- AnalyseNNNS.py
+++ AnalyseNNNS.py
@@ -4,19 +4,24 @@
 @author: Maria Laura Gabriel Kuniyoshi.
 """
 import argparse
+import sys
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
 
 if __name__ == '__main__':
-    message = """Calculates summary scores and produces boxplots from a table
+    MESSAGE = """Calculates summary scores and produces boxplots from a table
     with NeoNatal Neurobehavioral Scale results. """
-    parser = argparse.ArgumentParser(description=message)
+    parser = argparse.ArgumentParser(description=MESSAGE)
     parser.add_argument("file",
                         type=str,
-                        help="""Name of the file containing the NNNS raw data.""")
+                        help="Name of the file containing the NNNS raw data.")
     parser.add_argument("--output_filename",
                         type=str,
                         help="Name on the output files.",
@@ -31,6 +36,10 @@ if __name__ == '__main__':
                         help="Produces boxplot images. Default is True",
                         default=True)
     args = parser.parse_args()
+
+
+#############################################################################
+# Importing the file and detecting missing/invalid data.
 
 
 def import_excel(file):
@@ -53,8 +62,6 @@ def import_excel(file):
     # Convert data types.
     df = df.astype('Int16', errors='ignore')
 
-    # The ones marked as 95 - 99 are missing data. This will set them as NaN.
-    df = df.mask(df >= 90)
     return df
 
 
@@ -72,7 +79,7 @@ def import_csv(file):
             return int(x)
         except:
             return x
-    
+
     df.iloc[:, 0] = df.iloc[:, 0].apply(convers)
     df = df.set_index(list(df.columns)[0:2])
 
@@ -83,19 +90,182 @@ def import_csv(file):
     # Convert data types.
     df = df.astype('Int16', errors='ignore')
 
-    # The ones marked as 95 - 99 are missing data. This will set them as NaN.
-    df = df.mask(df >= 90)
+    return df
+
+
+def check_range(df):
+    """
+    Check if the values are accoring to the description from Lester 2004,
+    The Neonatal Intensive Care Unit Network Neurobehavioral Scale Procedures.
+
+    If it is not accordingly, substitute for NaN. 
+    """
+
+    valid_scores = {1: [1, 2, 3, 4, 5, 6],
+                    2: [1, 2, 3, 4, 5, 6, 7, 8, 9, 95, 96, 97, 98, 99],
+                    3: [1, 2, 3, 4, 5, 6, 7, 8, 9, 95, 96, 97, 98, 99],
+                    4: [1, 2, 3, 4, 5, 6, 7, 8, 9, 95, 96, 97, 98, 99],
+                    5: [1, 2, 3, 4, 5, 98, 99],
+                    6: [1, 2, 3, 4, 98, 99],
+                    7: [],
+                    '7a': [1, 2, 0, np.nan],
+                    '7b': [1, 2, 0, np.nan],
+                    '7c': [1, 2, 0, np.nan],
+                    '7d': [1, 2, 0, np.nan],
+                    8: [1, 2, 3, 98, 99],
+                    9: [1, 2, 3, 4, 5, 6, 7, 8, 9, 95, 96, 97, 98, 99],
+                    10: [1, 2, 3, 4, 98, 99],
+                    11: [1, 2, 3, 4, 98, 99],
+                    12: [1, 2, 3, 4, 98, 99],
+                    13: [1, 2, 3, 4, 5, 98, 99],
+                    14: [1, 2, 3, 4, 5, 98, 99],
+                    15: [1, 2, 3, 4, 5, 98, 99],
+                    16: [1, 2, 3, 4, 5, 6, 98, 99],
+                    17: [1, 2, 3, 4, 98, 99],
+                    18: [1, 2, 3, 4, 5, 98, 99],
+                    19: [1, 2, 3, 4, 5, 98, 99],
+                    20: [1, 2, 3, 4, 5, 98, 99],
+                    21: [1, 2, 3, 4, 5, 98, 99],
+                    22: [1, 2, 3, 4, 5, 6, 7, 98, 99],
+                    23: [1, 2, 3, 4, 98, 99],
+                    24: [1, 2, 3, 4, 5, 98, 99],
+                    25: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 , 98, 99],
+                    26: [1, 2, 3, 98, 99],
+                    27: [1, 2, 3, 4, 5, 6, 7, 98, 99],
+                    28: [1, 2, 3, 4, 5, 6, 98, 99],
+                    29: [1, 2, 3, 4, 98, 99],
+                    30: [1, 2, 3, 4, 98, 99],
+                    31: [1, 2, 0, 98, 99],
+                    32: [1, 2, 3, 4, 5, 6, 98, 99],
+                    33: [1, 2, 3, 4, 5, 6, 7, 8, 9, 97, 98, 99],
+                    34: [1, 2, 3, 4, 5, 6, 7, 8, 9, 97, 98, 99],
+                    35: [1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 99],
+                    36: [1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 99],
+                    37: [1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 99],
+                    38: [1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 99],
+                    39: [1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 99],
+                    40: [1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 99],
+                    41: [1, 2, 3, 4, 5, 6, 98, 99],
+                    42: [1, 2, 3, 4, 98, 99],
+                    43: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 98, 99],
+                    44: [1, 2, 3, 4, 98, 99],
+                    45: [1, 2, 3, 4, 5, 98, 99],
+                    46: [],
+                    '46a': [1, 2, 0, np.nan],
+                    '46b': [1, 2, 0, np.nan],
+                    '46c': [1, 2, 0, np.nan],
+                    '46d': [1, 2, 0, np.nan],
+                    '46e': [1, 2, 0, np.nan],
+                    '46f': [1, 2, 0, np.nan],
+                    '46g': [1, 2, 0, np.nan],
+                    '46h': [1, 2, 0, np.nan],
+                    '46i': [1, 2, 0, np.nan],
+                    47: [1, 2, 3, 4, 5, 6, 7, 8, 9, 98],
+                    48: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 98],
+                    49: [1, 2, 3, 4, 5, 6, 7, 8, 9, 98],
+                    50: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 98, 99],
+                    51: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    52: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                    53: [1, 2, 3, 4, 5, 6, 7, 8, 9, 98],
+                    54: [1, 2, 3, 4, 5, 6, 98],
+                    55: [1, 2, 3, 4, 5, 6, 98],
+                    56: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    57: [1, 2, 3, 4, 5, 6, 7, 8, 9, 98],
+                    58: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+                    59: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    60: [1, 2, 3, 4, 5, 6, 7, 8, 9, 98],
+                    61: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                    62: [1, 2, 3, 4, 5, 6],
+                    63: [1, 2, 3, 4, 5, 6],
+                    64: [1, 2, 3, 4, 5, 6],
+                    65: [1, 2, 3, 4, 5, 6],
+                    66: [1, 2, 0, np.nan],
+                    67: [1, 2, 0, np.nan],
+                    68: [1, 2, 0, np.nan],
+                    69: [1, 2, 0, np.nan],
+                    70: [1, 2, 0, np.nan],
+                    71: [1, 2, 0, np.nan],
+                    72: [1, 2, 0, np.nan],
+                    73: [1, 2, 0, np.nan],
+                    74: [1, 2, 0, np.nan],
+                    75: [1, 2, 0, np.nan],
+                    76: [1, 2, 0, np.nan],
+                    '77a': [1, 2, 0, np.nan],
+                    '77b': [1, 2, 0, np.nan],
+                    78: [1, 2, 0, np.nan],
+                    79: [1, 2, 0, np.nan],
+                    80: [1, 2, 0, np.nan],
+                    81: [1, 2, 0, np.nan],
+                    82: [1, 2, 0, np.nan],
+                    83: [1, 2, 0, np.nan],
+                    84: [1, 2, 0, np.nan],
+                    85: [1, 2, 0, np.nan],
+                    86: [1, 2, 0, np.nan],
+                    87: [1, 2, 0, np.nan],
+                    88: [1, 2, 0, np.nan],
+                    89: [1, 2, 0, np.nan],
+                    90: [1, 2, 0, np.nan],
+                    91: [1, 2, 0, np.nan],
+                    92: [1, 2, 0, np.nan],
+                    93: [1, 2, 0, np.nan],
+                    94: [1, 2, 0, np.nan],
+                    95: [1, 2, 0, np.nan],
+                    96: [1, 2, 0, np.nan],
+                    97: [1, 2, 0, np.nan],
+                    98: [1, 2, 0, np.nan],
+                    99: [1, 2, 0, np.nan],
+                    100: [1, 2, 0, np.nan],
+                    101: [1, 2, 0, np.nan],
+                    102: [1, 2, 0, np.nan],
+                    103: [1, 2, 0, np.nan],
+                    104: [1, 2, 0, np.nan],
+                    105: [1, 2, 0, np.nan],
+                    106: [1, 2, 0, np.nan],
+                    107: [1, 2, 0, np.nan],
+                    108: [1, 2, 0, np.nan],
+                    109: [1, 2, 0, np.nan],
+                    110: [1, 2, 0, np.nan],
+                    111: [1, 2, 0, np.nan],
+                    112: [1, 2, 0, np.nan],
+                    113: [1, 2, 0, np.nan],
+                    114: [1, 2, 0, np.nan],
+                    115: [1, 2, 0, np.nan]}
+
+    for i in df.iterrows():
+        row = i[1].astype('int', errors='ignore')
+        variable = i[0]
+
+        if variable[0] in [7, 46]:
+            continue
+
+        valid_value = row.isin(valid_scores[variable[0]])
+        invalid = list(valid_value[valid_value is False].index)
+
+        for baby in invalid:
+            print(f'Invalid or missing score for the variable {variable[0]}, sample {baby}')
+            df.loc[variable, baby] = np.nan
+    print('Replaced all invalid or missing scores with NaN')
     return df
 
 
 def import_data(extension, file):
     if extension == 'xlsx':
-        return import_excel(file)
+        df =  import_excel(file)
     elif extension == 'csv':
-        return import_csv(file)
+        df = import_csv(file)
     else:
         print('Unsupported extension')
-        exit()
+        sys.exit()
+
+    df = check_range(df)
+
+    # The ones marked as 95 - 99 are missing data. This will set them as NaN.
+    df = df.mask(df.isin([95, 96, 97, 98, 99]))
+
+    return df
+
+##############################################################################
+# Calculate scores
 
 
 def binarify(df, row_name, condition_pos, condition_neg):
@@ -107,11 +277,13 @@ def binarify(df, row_name, condition_pos, condition_neg):
     condition_neg: Condition where class is 0.
     """
     x = df.loc[[row_name]]
-    condition_pos = '(pd.notna(x)) &' + condition_pos
-    condition_neg = '(pd.notna(x)) &' + condition_neg
-    x = x.mask(eval(condition_pos), 1)
-    x = x.mask(eval(condition_neg), 0)
-    df.loc[[row_name]] = x
+    x = x.astype('object')
+
+    np_x = np.select([eval(condition_pos), eval(condition_neg), pd.isna(x)],
+                     [1                  , 0                  , np.nan    ])
+
+    df.loc[[row_name]] = pd.DataFrame(np_x, columns=x.columns, index=x.index)
+
     return df
 
 
@@ -136,7 +308,7 @@ def narousal(df):
     return items.mean()
 
 
-def nselfre3(df):
+def nselfre3(df):  # Minor differences
     """Calculate regulation score."""
     items = df.loc[[25, 33, 34, 43, 47, 48, 49,
                     50, 52, 56, 57, 58, 59, 60, 61]]
@@ -148,17 +320,17 @@ def nselfre3(df):
         57: {9: np.nan},
         58: {1: np.nan, 2: 7, 4: 5, 5: 6, 6: 4, 7: 3, 8: 2, 9: 1, 10: 3, 11: 1},
         59: {2: 5, 3: 4, 4: 3, 5: 2, 6: 2, 7: 1, 8: 1, 9: 1},
-        61: {6: 7, 7: 8, 8: 9, 9: 10}
+        61: {6: 7, 7: 8, 8: 9, 9: 10, 10:6}
         }).T
     rc_items.loc[[52]] = 13 - rc_items.loc[[52]]
     rc_items.loc[[56]] = 10 - rc_items.loc[[56]]
-    rc_items.loc[[57]] = 10 - rc_items.loc[[57]]
+    rc_items.loc[[57]] = 10 - rc_items.loc[[57]].replace(9, np.nan)
 
     rc_items = rc_items.dropna(axis=1, thresh=10)
     return rc_items.mean()
 
 
-def nhandle(df):
+def nhandle(df): # Minor differences
     """Calculate handling score."""
     items = df.loc[['46a', '46b', '46c', '46d', '46e', '46f', '46g', '46h']]
     rc_items = items.replace({2: 0})
@@ -183,21 +355,13 @@ def nqmove(df):
     return rc_items.mean()
 
 
-def nhexctot(df):
+def nhexctot(df):  # Minor differences
     """Calculate excitability score."""
     items = df.loc[[33, 34, 48, 49, 50, 51, 52,
                     53, 54, 55, 56, 57, 58, 59, 60]]
 
     items = items.reset_index(level=1, drop=True)
     itemsT = items.T
-
-    itemsT.loc[(itemsT[50].isna()) &
-               (itemsT[60].isna()) &
-               (itemsT[51] > 0) &
-               (itemsT[51] < 7),
-               [48, 60]] = 0
-
-    items = itemsT.T
 
     cond = [[33, '(x>=1) & (x<3)', '(x>=3)'],
             [34, '(x>=1) & (x<3)', '(x>=3)'],
@@ -217,10 +381,20 @@ def nhexctot(df):
     for i in cond:
         items = binarify(items, *i)
 
+    itemsT2 = items.T
+
+    itemsT2.loc[(itemsT[50].isna()) &
+               (itemsT[60].isna()) &
+               (itemsT[51] > 0) &
+               (itemsT[51] < 7),
+               [48, 60]] = 0
+
+    items = itemsT2.T
+
     return items.sum()
 
 
-def nbadref1(df):
+def nbadref1(df):  # Minor differences
     """Calculate non-optimal reflexes score."""
     items = df.loc[[10, 11, 12, 21, 22, 23, 26, 27,
                     28, 30, 32, 41, 42, 44, 45]]
@@ -231,7 +405,7 @@ def nbadref1(df):
             [21, '(x == 3)', 'x.isin([1, 2, 4, 5])'],
             [22, '(x == 3)', 'x.isin([1, 2, 4, 5, 6, 7])'],
             [23, '(x == 3)', 'x.isin([1, 2, 4])'],
-            [26, '(x == 3)', 'x.isin([1, 2, 4])'],
+            [26, '(x == 3)', 'x.isin([1, 2])'],
             [27, '(x == 4)', 'x.isin([1, 2, 3, 5, 6, 7])'],
             [28, '(x == 3)', 'x.isin([1, 2, 4])'],
             [30, '(x == 3)', 'x.isin([1, 2, 4])'],
@@ -248,20 +422,20 @@ def nbadref1(df):
     return items.apply((lambda x: (x == 0).sum()))
 
 
-def ndeprtot(df):
+def ndeprtot(df):  # Minor differences
     """Calculate lethargy score."""
     items = df.loc[[25, 35, 36, 37, 38, 39, 40, 43,
                     47, 48, 51, 52, 54, 55, 53]]
     items = items.reset_index(level=1, drop=True)
 
     itemsT = items.T
-    itemsT.loc[((itemsT[47].isna()) | (itemsT[47].isin([1, 2])))
+    itemsT.loc[(itemsT[47].isin([np.nan, 1, 2]))
                &
-               (itemsT[[35, 36, 37, 38, 39, 40]].isnull().any(axis=1)),
+               (itemsT[[35, 36, 37, 38, 39, 40]].isna().any(axis=1)),
                ['ALOR_SUB']] = 1
     itemsT['ALOR_SUB'] = itemsT['ALOR_SUB'].fillna(0)
 
-    for i in [35, 36, 37, 38, 39, 40]:
+    for i in [35, 36, 37, 38, 39, 40, 47]:
         itemsT.loc[(itemsT['ALOR_SUB'] == 1) & (itemsT[i].isna()), i] = 1
     items = itemsT.T
     items.drop(index=['ALOR_SUB'])
@@ -278,28 +452,17 @@ def nasymtot(df):
     """Calculate asymmetrical reflexes score."""
     items = df.loc[[10, 11, 12, 13, 14, 15, 16, 17,
                     18, 19, 20, 21, 23, 26, 27, 29]]
-    items = items.reset_index(level=1, drop=True)
+    items = items.dropna(axis=1, how='any')
 
-    rc_items = items.T.replace({
-                                10: {1: 1, 2: 1, 3: 1, 4: 1},
-                                11: {1: 1, 2: 1, 3: 1, 4: 1},
-                                12: {1: 1, 2: 1, 3: 1, 4: 1},
-                                13: {1: 1, 2: 1, 3: 1, 4: 1},
-                                14: {1: 1, 2: 1, 3: 1, 4: 1},
-                                15: {1: 1, 2: 1, 3: 1, 4: 1},
-                                16: {1: 1, 2: 1, 3: 1, 4: 1},
-                                17: {1: 1, 2: 1, 3: 1, 4: 1},
-                                18: {1: 1, 2: 1, 3: 1, 4: 1},
-                                19: {1: 1, 2: 1, 3: 1, 4: 1},
-                                20: {1: 1, 2: 1, 3: 1, 4: 1},
-                                21: {1: 1, 2: 1, 3: 1, 4: 1},
-                                23: {1: 1, 2: 1, 3: 1, 4: 1},
-                                26: {1: 1, 2: 1, 3: 1, 4: 1},
-                                27: {1: 1, 2: 1, 3: 1, 4: 1},
-                                29: {1: 1, 2: 1, 3: 1, 4: 1}
-                                }).T
-    rc_items = items.dropna(axis=1, how='any')
-    return rc_items.sum()
+    cond = [[i, '(x == 5)', 'x.isin([1, 2, 3, 4])']
+            for i in [10, 11, 12, 13, 14, 15, 16, 17,
+                      18, 19, 20, 21, 23, 26, 27, 29]]
+    for i in cond:
+        items = binarify(items, *i)
+
+    items = items.groupby(level=0).max()
+
+    return items.sum()
 
 
 def nhypetot(df):
@@ -358,6 +521,10 @@ def nstress(df):
     return items.mean()
 
 
+##############################################################################
+# Get results
+
+
 def summary_scores(df):
     """
     Calculate all summary scores for the data.
@@ -407,7 +574,7 @@ def main():
     sum_table = summary_table(sum_scores)
     sum_table.to_csv(f'summary_scores_{output}.txt', sep='\t')
 
-    if args.boxplot == True:
+    if args.boxplot is True:
         boxplots(sum_scores, output)
 
 
